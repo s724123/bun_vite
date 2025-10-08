996575ea3c738a30d9311af99a8772013699c42f
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-10-08 15:26:42 +0800

    add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  // 必須確保 Tailwind 知道要去哪裡掃描您的類別名稱 (class names)
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 確保包含了所有使用 JSX/TSX 的檔案
+  ],
+
+  // 這是啟用 DaisyUI 插件的關鍵
+  plugins: [
+    require('daisyui'),
+  ],
+
+  // (可選) 您可以在這裡設定 DaisyUI 的主題
+  daisyui: {
+    themes: ["light", "dark", "cupcake"], // 確保您需要的顏色主題在這裡
+  },
+}
