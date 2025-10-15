6ad4e4e2dde122de72612cadde7a73f81dced9e3
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-10-15 17:59:17 +0800

    test jsx to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import Menu from './pages/Menu.tsx'
+import About from './pages/About.tsx'
+import Contact from './pages/Contact.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 建立路由配置
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,           // 預設子路由
+        element: <Home />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+])
+
+// 渲染應用程式
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
