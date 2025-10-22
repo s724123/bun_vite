c3a47df65570f1e185618b3807b462ac98b19a0b
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-10-22 17:47:04 +0800

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,12 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col bg-base-100">
       {/* 頁面標題列 */}
-
       <Header />
-
       {/* 主要內容區域 */}
       <main className="flex-1 container mx-auto px-4 py-8">
         {/* React Router 的 Outlet 會在此渲染對應的頁面元件 */}
-        <h1>App</h1>
         <Outlet />
       </main>
-
       {/* 頁面底部 */}
       <Footer />
     </div>
